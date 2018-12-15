ef7cb2399bbb206c15d93aa8ec2fb0082c2eadc0
Ali Rahmat Ismail <chiriku.ali.rhmat@gmail.com>
2018-12-15 08:16:36 +0700

    first commit

---
+++ app/src/main/java/chrikualii/info/catalogmovie/NowPlayingFragment.java
@@ -0,0 +1,37 @@
+package chrikualii.info.catalogmovie;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NowPlayingFragment extends Fragment {
+
+
+    public NowPlayingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_now_playing, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/chrikualii/info/catalogmovie/ViewPagerAdapter.java
@@ -0,0 +1,38 @@
+package chrikualii.info.catalogmovie;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by chirikualii on 13/12/18
+ * github.com/chirikualii
+ */
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        Fragment fragment = null;
+        switch (i){
+            case 0:
+                fragment = new NowPlayingFragment();
+                break;
+            case 1:
+                fragment = new UpcomingFragment();
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+
+}
